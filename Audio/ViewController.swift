ae2c9e7c603ae43b2d35f6e768f0b7a9c32b8c0c
jeongwon <choigarden@nate.com>
2022-09-04 02:22:46 +0900

    오디오 재생을 위한 초기화하기

--- Audio/ViewController.swift
+++ Audio/ViewController.swift
@@ -6,21 +6,21 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+    
+    var audioPlayer : AVAudioPlayer!
+    var audioFile : URL!
+    let MAX_VOLUME : Float = 10.0
+    var progressTimer : Timer!
 
     @IBOutlet var pvProgressPlay: UIProgressView!
-    
     @IBOutlet var lblCurrentTime: UILabel!
-    
     @IBOutlet var lblEndTime: UILabel!
-    
     @IBOutlet var btnPlay: UIButton!
-    
     @IBOutlet var btnPause: UIButton!
-    
     @IBOutlet var btnStop: UIButton!
-    
     @IBOutlet var slVolume: UISlider!
     
     
@@ -30,6 +30,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        audioFile = Bundle.main.url(forResource: "Sicilian_Breeze", withExtension: "mp3")
+        initPlay()
+        
+    }
+    
+    func initPlay() {
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: audioFile)
+        } catch let error as NSError {
+            print("Error-initPlay : \(error)")
+        }
+        slVolume.maximumValue = MAX_VOLUME
+        slVolume.value = 1.0
+        pvProgressPlay.progress = 0
+        
+        audioPlayer.delegate = self
+        audioPlayer.prepareToPlay()
+        audioPlayer.volume = slVolume.value
     }
 
 
